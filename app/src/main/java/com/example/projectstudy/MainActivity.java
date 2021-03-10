e8c53a6fa34d716766931d9bc35b331bcd819f16
Dat Do <datdt@imt-soft.com>
2021-03-10 15:38:08 +0700

    3. DAGGER: FIELD INJECTION

--- app/src/main/java/com/example/projectstudy/MainActivity.java
+++ app/src/main/java/com/example/projectstudy/MainActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
-    private Car car;
+    @Inject Car car;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CarComponent carComponent = DaggerCarComponent.create();
-        car = carComponent.getCar();
+        carComponent.inject(this);
         car.drive();
     }
 }
