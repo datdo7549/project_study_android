8de7a20873402dea200eaa135a68b9ec323a9aeb
Dat Do <datdt@imt-soft.com>
2021-03-12 13:40:05 +0700

    5. DAGGER: MODULES & PROVIDER METHODS. We don't own this class so we can't annotate it with @Inject

--- app/src/main/java/com/example/projectstudy/CarComponent.java
+++ app/src/main/java/com/example/projectstudy/CarComponent.java
@@ -2,7 +2,7 @@ package com.example.projectstudy;
 
 import dagger.Component;
 
-@Component
+@Component (modules = WheelsModule.class)
 public interface CarComponent {
     void inject(MainActivity mainActivity);
 }
---
+++ app/src/main/java/com/example/projectstudy/Rims.java
@@ -0,0 +1,5 @@
+package com.example.projectstudy;
+
+public class Rims {
+    //We don't own this class so we can't annotate it with @Inject
+}
