26f00a94f3b37726719bf639070deaa70a5f0b8b
Dat Do <datdt@imt-soft.com>
2021-03-12 11:38:18 +0700

    4. DAGGER: METHOD INJECTION. @Inject: Constructor -> field -> method

--- app/src/main/java/com/example/projectstudy/Car.java
+++ app/src/main/java/com/example/projectstudy/Car.java
@@ -7,16 +7,37 @@ import javax.inject.Inject;
 public class Car {
     private static final String TAG = "Car";
 
-    private Engine engine;
-    private Wheels wheels;
+    @Inject
+    public Engine engine;
+    @Inject
+    public Wheels wheels;
 
     @Inject
-    public Car(Engine engine, Wheels wheels) {
-        this.engine = engine;
-        this.wheels = wheels;
+    public Car() {
     }
-    
+
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
+    }
+
     public void drive() {
         Log.d(TAG, "driving...");
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Wheels getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(Wheels wheels) {
+        this.wheels = wheels;
     }
 }
--- app/src/main/java/com/example/projectstudy/MainActivity.java
+++ app/src/main/java/com/example/projectstudy/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.projectstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
-    @Inject Car car;
+    private static final String TAG = "MainActivity";
+    @Inject
+    Car car;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
